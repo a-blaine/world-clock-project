e8ce91d2857c96fe75a93050530f3ccaf5691104
a-blaine <ai.blaine4@protonmail.com>
2023-03-23 17:34:32 +0000

    Fixed the bug in Javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
 function updateTime() {
   let london = document.querySelector("#london");
   if (london) {
-    let londonTime = moment().tz("Europe/London");
     let londonTimeElement = london.querySelector(".time");
     let londonDateElement = london.querySelector(".date");
+    let londonTime = moment().tz("Europe/London");
 
     londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
     londonTimeElement.innerHTML = londonTime.format(
@@ -13,9 +13,9 @@ function updateTime() {
 
   let shanghai = document.querySelector("#shanghai");
   if (shanghai) {
-    let shanghaiTime = moment().tz("Asia/Shanghai");
     let shanghaiTimeElement = shanghai.querySelector(".time");
     let shanghaiDateElement = shanghai.querySelector(".date");
+    let shanghaiTime = moment().tz("Asia/Shanghai");
 
     shanghaiDateElement.innerHTML = shanghaiTime.format("MMMM Do YYYY");
     shanghaiTimeElement.innerHTML = shanghaiTime.format(
@@ -25,9 +25,9 @@ function updateTime() {
 
   let phoenix = document.querySelector("#phoenix");
   if (phoenix) {
-    let phoenixTime = moment().tz("America/Phoenix");
     let phoenixTimeElement = phoenix.querySelector(".time");
     let phoenixDateElement = phoenix.querySelector(".date");
+    let phoenixTime = moment().tz("America/Phoenix");
 
     phoenixDateElement.innerHTML = phoenixTime.format("MMMM Do YYYY");
     phoenixTimeElement.innerHTML = phoenixTime.format(
@@ -44,17 +44,19 @@ function updateCity(event) {
     }
     let cityName = cityTimezone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimezone);
-    let citiesContainerElement = document.querySelector("#cities");
-    citiesContainerElement.innerHTML = `<div class="city">
-          <div>
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+        <div>
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss:SS"
-          )}<small>${cityTime.format("A")}<small></div>
         </div>
-        <a href="/">All cities</a>`;
+        <div class="time">${cityTime.format(
+          "h:mm:ss:SS"
+        )}<small>${cityTime.format("A")}</small>
+        </div>
+    </div>    
+    <a href="/">All cities</a>`;
   }
 }
 
